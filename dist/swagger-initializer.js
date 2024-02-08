24ad7f164dec95a9d8ca82fbd70189a9e303d7b8
Bakondi Béla <100357503+bakondibela@users.noreply.github.com>
2024-02-08 23:35:36 +0100

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,8 +3,8 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
-    dom_id: '#swagger-ui',
+    url: "Eurostone.Webshop.Api.24.1.2.yaml",
+    dom_id: '#Eurostone.Webshop.Api.24.1.2.yaml',
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
