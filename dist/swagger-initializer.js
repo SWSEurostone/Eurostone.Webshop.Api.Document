5c4d5ffe0abca1fec3fd5a04b529fa94336fcb18
Bakondi Béla <100357503+bakondibela@users.noreply.github.com>
2025-06-17 14:02:06 +0200

    25.2.71. documentum

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -2,19 +2,34 @@ window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    url: "dist/Eurostone.Webshop.Api.Document.yaml",
-    dom_id: '#swagger-ui',
-    deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
-  });
+  window.ui = SwaggerUIBundle(
+    //   {
+    //   url: "dist/Eurostone.Webshop.Api.Document.yaml",
+    //   dom_id: '#swagger-ui',
+    //   deepLinking: true,
+    //   presets: [
+    //     SwaggerUIBundle.presets.apis,
+    //     SwaggerUIStandalonePreset
+    //   ],
+    //   plugins: [
+    //     SwaggerUIBundle.plugins.DownloadUrl
+    //   ],
+    //   layout: "StandaloneLayout"
+    // },
+      {
+          url: "dist/Eurostone.Webshop.Api.Document.25.2.71.yaml",
+          dom_id: '#swagger-ui',
+          deepLinking: true,
+          presets: [
+              SwaggerUIBundle.presets.apis,
+              SwaggerUIStandalonePreset
+          ],
+          plugins: [
+              SwaggerUIBundle.plugins.DownloadUrl
+          ],
+          layout: "StandaloneLayout"
+      }
+  );
 
   //</editor-fold>
 };
